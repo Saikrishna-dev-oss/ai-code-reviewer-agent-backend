fa8eea3b641dffd6301515137c439d67b4ffcec1
Mangali Sai Krishna <mangalisaikrishna003@gmail.com>
2026-06-29 23:20:55 +0530

    Add initial project files: .gitignore, README.md, main.py, and requirements.txt

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(title="AI Code Reviewer API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World. The AI Backend is running."}
+
+@app.post("/review")
+def mock_review():
+    
+    return {
+        "status": "success",
+        "review": "This is a mock AI code review. The files look structurally sound."
+    }
